5dc052a235ad1b53c404a296989277adc32b3e1d
Thiago Henrique Xavier Medeiros <thihxm@gmail.com>
2023-10-26 22:56:30 -0300

    feat: response headers + 404 error file

--- servidor.py
+++ servidor.py
@@ -3,24 +3,22 @@ import socket
 from pathlib import Path
 import os
 import pickle
+import datetime
+from zoneinfo import ZoneInfo
 
 # import thread module
 from _thread import *
 import threading
-from utils import compute_sha256
-
-
-print_lock = threading.Lock()
 
 # thread function
 def threaded(client: socket.socket):
     data = client.recv(4096)
     message = data.decode()
 
-    palavras = message.split()
+    request_headers = message.split()
+    print(request_headers)
 
-    file_data = convert_file_to_protocol(palavras[1])
-    print(file_data)
+    file_data = convert_file_to_protocol(request_headers[1])
     client.send(file_data)
     client.close()
 
@@ -28,32 +26,69 @@ def convert_file_to_protocol(file_name):
     file_path = f'./files_server{file_name}'
     try:
         with open(file_path, 'rb') as file:
-            file_as_path = Path(file.name)
             file_content = file.read()
-            file_checksum = compute_sha256(file_content)
-            file_size = file_as_path.stat().st_size
+            file_size = len(file_content)
 
-        return b"HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n" + file_content
+            last_modified_timestamp = os.path.getmtime(file_path)
+            last_modified_date = datetime.datetime.fromtimestamp(last_modified_timestamp, ZoneInfo("Etc/GMT")).strftime('%a, %d %b %Y %H:%M:%S GMT')
+
+            if file_path.endswith('.jpg') or file_path.endswith('.jpeg'):
+                content_type = "image/jpeg"
+            elif file_path.endswith('.png'):
+                content_type = "image/png"
+            elif file_path.endswith('.html'):
+                content_type = "text/html"
+            else:
+                content_type = "application/octet-stream"
+
+            headers = [
+                "HTTP/1.1 200 OK",
+                f"Date: {datetime.datetime.utcnow().strftime('%a, %d %b %Y %H:%M:%S GMT')}",
+                f"Last-Modified: {last_modified_date}",
+                f"Server: ServidorTCPPython/1.0",
+                f"Content-Type: {content_type}",
+                f"Content-Length: {file_size}",
+                "Connection: close",
+                "\r\n"
+            ]
+
+            header = "\r\n".join(headers)
+            return header.encode() + file_content
 
     except FileNotFoundError:
         print(f'File {file_path} not found')
-        return b"HTTP/1.1 404 Not Found\r\nContent-Type: text/html\r\n\r\n"
+        file_path = f'./error/404.html'
+        with open(file_path, 'rb') as file:
+            file_content = file.read()
+            file_size = len(file_content)
+
+            headers = [
+                "HTTP/1.1 404 Not Found",
+                f"Date: {datetime.datetime.utcnow().strftime('%a, %d %b %Y %H:%M:%S GMT')}",
+                "Server: ServidorTCPPython/1.0",
+                "Content-Type: text/html",
+                f"Content-Length: {file_size}",
+                "Connection: close",
+                "\r\n"
+            ]
+            header = "\r\n".join(headers)
+            return header.encode() + file_content
 
 def Main():
-    host = "127.0.1.1"
+    host = "127.0.0.1"
 
     #host = "10.0.2.15"
 
     # reserve a port on your computer
     # in our case it is 12345 but it
     # can be anything
-    port = 12346
+    port = 12345
     socket_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     socket_server.bind((host, port))
     hostname = socket.gethostname()
     ip_address = socket.gethostbyname_ex(hostname)
     print("socket binded to port", port)
-    print(f"Server IP is {ip_address}")
+    print(f"Server IP is {ip_address[2]}")
 
     # put the socket into listening mode
     socket_server.listen(5)
@@ -65,8 +100,6 @@ def Main():
         # establish connection with client
         client, addr = socket_server.accept()
 
-        # lock acquired by client
-        # print_lock.acquire()
         print('Connected to :', addr[0], ':', addr[1])
 
         # Start a new thread and return its identifier
